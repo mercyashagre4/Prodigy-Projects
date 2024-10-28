b8db1713c22343656828032f774ae3434ecba82a
mercy <118175416+mercyashagre4@users.noreply.github.com>
2024-10-28 14:17:53 -0700

    Update main.js

--- Task-5/Weather Forecast/Weather App/assets/main.js
+++ Task-5/Weather Forecast/Weather App/assets/main.js
@@ -9,8 +9,8 @@ const API_KEY = 'bd5e378503939ddaee76f12ad7a97608';
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
         return `<div class="details">
-                    <h2>${cityInput} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-                    <h4>Temperature: ${(weatherItem.main.temp - 273.15.toFixed(2))}°C</h4>
+                    <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+                    <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
                     <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
                 </div>
@@ -24,7 +24,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
         return `<li class="card">
                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-                    <h4>Temp: ${weatherItem.main.temp - 273.15.toFixed(2)}°C</h4>
+                    <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
                     <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
                     <h4>Humidity: ${weatherItem.main.humidity}%</h4>
                 </li>`;
